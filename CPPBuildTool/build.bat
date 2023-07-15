b4f797003e15da713cca0d192f38c2621b03003f
5c0ded <selfcoded.name@gmail.com>
2023-07-15 21:50:59 +1000

    move to solid visual studio solution rite now

--- CPPBuildTool/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo "Started build process..."
-gcc -std=c++17 -Wall -Werror -pedantic -O3 -lstdc++ -o bt1 Src/Main.cpp
-echo "Build end."
---
+++ InterpreterRuntime/Source/Start.cc
@@ -0,0 +1,5 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE _unused0, LPSTR lpCmdLine, int _unused1) {
+	return 0;
+}
